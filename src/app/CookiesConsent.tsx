fc6d055759d2ae8426b8b6dd07faf6e3d15666de
sutra-rudi <rudi@sutra.hr>
2024-05-15 13:45:14 +0200

    cookie consent

--- src/app/CookiesConsent.tsx
+++ src/app/CookiesConsent.tsx
@@ -3,6 +3,22 @@
 import React from 'react';
 import CookieConsent from 'react-cookie-consent';
 
-const CookiesConsent = () => <CookieConsent />;
+const clientW = window.innerWidth;
+
+const CookiesConsent = () => (
+  <CookieConsent
+    debug
+    buttonText={'Razumjem'}
+    style={{ background: '#17AEC2', maxWidth: clientW > 768 ? '768px' : '100%', placeItems: 'flex-end' }}
+    buttonStyle={{ background: '#F5FAFF', borderRadius: '6px' }}
+  >
+    <h2 className='xl:text-2xl text-lg'>Pozdrav!</h2>
+
+    <p className='xl:text-base text-sm text-balance'>
+      Ova stranica koristi kolačiće kako bi osigurala bolje korisničko iskustvo. Korištenjem ove stranice pristajete na
+      upotrebu kolačića u skladu s našom Pravilima o kolačićima. Hvala vam na posjeti!
+    </p>
+  </CookieConsent>
+);
 
 export default CookiesConsent;
