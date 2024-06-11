74066433d7b7db79beca9034bcc01c2e04e7411a
sutra-rudi <rudi@sutra.hr>
2024-06-11 16:30:53 +0200

    dinko cache

--- src/app/operacije/[title]/page.tsx
+++ src/app/operacije/[title]/page.tsx
@@ -21,7 +21,7 @@ export default async function Operacije({
     body: JSON.stringify({
       query: getDinkoOperacijeQuery,
     }),
-    cache: 'no-store',
+    cache: 'no-cache',
   });
 
   const pageContent = await getDinkoOperacije.json();
